6cb5485df4079f0ff761b96bfcf1822d8e443b87
George Carder <georgercarder@gmail.com>
2018-10-29 06:40:36 -0700

    built and decomposed quaternion.. key was to use pointers for dickson..

--- dickson.h
+++ dickson.h
@@ -3,36 +3,38 @@ template<typename T>
 
 class Dickson {
   public:
-    T Re;
-    T Im;
+    T *Re;
+    T *Im;
 
   public:
-    Dickson(){};
+    Dickson(){
+      cout << "new di.." << endl;
+    };
 
-    Dickson(T a, T b){
+    Dickson(T *a, T *b){
       this->Re = a;
       this->Im = b;
+
+      cout << "new dickson" << endl;
     };
   
-    Dickson(const Dickson &obj){};
-
     ~Dickson(){};
 
-    void setRe(T a){
+    /*void setRe(T a){
       this->Re = a;
     };
 
     void setIm(T b){
       this->Im = b;
-    };
+    };*/
 
-    T getRe(){
-      T re = this->Re;
+    T* getRe(){
+      T *re = this->Re;
       return re;
     };
 
-    T getIm(){
-      T im = this->Im;
+    T* getIm(){
+      T *im = this->Im;
       return im;
     };
 
--- main.cpp
+++ main.cpp
@@ -9,23 +9,49 @@ using namespace std;
 
 int main() {
 
-  Complex comp[4];
+  int ent[8] = {3,4,5,2,6,7,8,9};
 
-  for (int i = 0; i < 4; i++) {
-    int a = rand();
-    int b = rand();
-    comp[i] = Complex(a, b);
-  };
+  Complex com(&ent[0], &ent[1]), com2(&ent[2], &ent[3]);
 
-  Quaternion quat[2];
+  int* comr = com.getRe();
+  int* comi = com.getIm();
+  int* com2r = com2.getRe();
+  int* com2i = com2.getIm();
 
-  for (int i = 0; i < 2; i++) {
-    quat[i] = Quaternion(comp[i], comp[i+1]);
-  };
+  cout << *comr << "\t" << *comi << endl;
+  cout << *com2r << "\t" << *com2i << endl;
 
-  Octonion oct(quat[0], quat[1]);
- 
+  ////
+  
+  Quaternion qua(&com, &com2);
 
+  Complex *c1, *c2;
+  c1 = qua.getRe();
+  c2 = qua.getIm();
+
+  Complex a1, a2;
+  a1 = *c1;
+  a2 = *c2;
+
+  int *a1r, *a1i, *a2r, *a2i;
+  a1r = a1.getRe();
+  a1i = a1.getIm();
+  a2r = a2.getRe();
+  a2i = a2.getIm();
+  
+
+  cout << *a1r << "\t" << *a1i << endl
+    << *a2r << "\t" << *a2i << endl;
+  
+  // up to this point.. built and dissassembled quaternion.
+  
+  //
+
+
+  
+  /*
+  Octonion oct(quat1, quat2);
+*/
   return 0;
 }
 
