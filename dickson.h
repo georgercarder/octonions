52bc3e2e507cd9bfc59c3e4d2c728c8b62cdb27b
George Carder <georgercarder@gmail.com>
2018-10-30 14:09:42 -0700

    overloaded << .. Thanks Tobi

--- dickson.h
+++ dickson.h
@@ -1,20 +1,27 @@
 
+using std::ostream;
+
 template<typename T>
 
 class Dickson {
   public:
-    T *Re;
-    T *Im;
+    T Re;
+    T Im;
 
   public:
     Dickson(){
-      cout << "new di.." << endl;
+      //cout << "new di.." << endl;
     };
 
-    Dickson(T *a, T *b){
+    Dickson(T a, T b){
       this->Re = a;
       this->Im = b;
-      cout << "new dickson" << endl;
+      //cout << "new dickson" << endl;
+    };
+
+    Dickson(const Dickson& d){
+      this->Re = d.Re;
+      this->Im = d.Im;
     };
   
     ~Dickson(){};
@@ -27,58 +34,67 @@ class Dickson {
       this->Im = b;
     };*/
 
-    T* getRe(){
-      T *re = this->Re;
+    T getRe(){
+      T re = this->Re;
       return re;
     };
 
-    T* getIm(){
-      T *im = this->Im;
+    T getIm(){
+      T im = this->Im;
       return im;
     };
 
     Dickson operator*(Dickson &_B){
-      T *c = _B.getRe();
+     /* T *c = _B.getRe();
       T *d = _B.getIm();
+*/
+      T c = _B.Re;
+      T d = _B.Im;
 
       // need to be sure conjugate is being accounted for
 
-      T A1 = *(this->Re) * *c;// - *(this->Im) * *d;
-      T B1 = *(this->Re) * *d;// + *(this->Im) * *c;
+      T A1 = this->Re * c;// - *(this->Im) * *d;
+      T B1 = this->Re * d;// + *(this->Im) * *c;
 
-      T A2 = *(this->Im) * *d;
-      T B2 = *(this->Im) * *c;
+      T A2 = this->Im * d;
+      T B2 = this->Im * c;
 
       T A = A1 - A2;
       T B = B1 + B2;
     //  cout << A << "\t" << B << endl;
 
-      Dickson res(&A, &B);
+      Dickson res(A, B);
 
       return res;
     };
 
     Dickson operator+(Dickson &_B){
-      T *c = _B.getRe();
-      T *d = _B.getIm();
 
-      T A = *(this->Re) + *c;
-      T B = *(this->Im) + *d;
+      T A = this->Re + _B.Re;
+      T B = this->Im + _B.Im;
 
-      Dickson res(&A, &B);
+      Dickson res(A, B);
 
       return res;
     };
 
     Dickson operator-(Dickson &_B){
-      T *c = _B.getRe();
-      T *d = _B.getIm();
 
-      T A = *(this->Re) - *c;
-      T B = *(this->Im) - *d;
+      T A = this->Re - _B.Re;
+      T B = this->Im - _B.Im;
 
-      Dickson res(&A, &B);
+      Dickson res(A, B);
 
       return res;
     };
+
 };
+
+template <typename T>
+    ostream& operator<<(ostream& os, Dickson<T> d) {
+      os << d.Re
+        << "  "
+        << d.Im;
+
+      return os;
+    };
--- main.cpp
+++ main.cpp
@@ -11,69 +11,41 @@ int main() {
 
   int ent[8] = {3,1,5,2,1,4,3,2};
 
-  Complex com(&ent[0], &ent[1]), com2(&ent[2], &ent[3]),
-          com3(&ent[4], &ent[5]), com4(&ent[6], &ent[7]);
+  Complex com(ent[0], ent[1]), com2(ent[2], ent[3]),
+          com3(ent[4], ent[5]), com4(ent[6], ent[7]);
 
-  int* comr = com.getRe();
-  int* comi = com.getIm();
-  int* com2r = com2.getRe();
-  int* com2i = com2.getIm();
-
-  cout << *comr << "\t" << *comi << endl;
-  cout << *com2r << "\t" << *com2i << endl;
+  cout << "Complex " << com << endl
+        << "Complex " << com2 << endl;  
 
   ////
   
-  Quaternion qua(&com, &com2),
-             qua2(&com3, &com4);
+  Quaternion qua(com, com2),
+             qua2(com3, com4);
 
-  Complex *c1, *c2;
-  c1 = qua.getRe();
-  c2 = qua.getIm();
-
-  Complex a1, a2;
-  a1 = *c1;
-  a2 = *c2;
-
-  int *a1r, *a1i, *a2r, *a2i;
-  a1r = a1.getRe();
-  a1i = a1.getIm();
-  a2r = a2.getRe();
-  a2i = a2.getIm();
-  
-  cout << "quat.. entries" << endl;
-  cout << *a1r << "\t" << *a1i << endl
-    << *a2r << "\t" << *a2i << endl;
+  cout << "Quaternion " << qua << endl;
   
   // up to this point.. built and dissassembled quaternion.
   // now test complex mult
 
   Complex comm = com * com2;
-  int commr = *comm.getRe();
-  int commi = *comm.getIm();
-
-  cout << commr << "\t" << commi << endl;
+  
+  cout << comm << endl;
   // up to this point.. complex multiplication appears to work.
 
   Complex csum = com + com2;
-  int csumr = *csum.getRe();
-  int csumi = *csum.getIm();
-
-  cout << csumr << "\t" << csumi << endl;
+  
+  cout << csum << endl;
   //
   Complex csub = com - com2;
-  int csubr = *csub.getRe();
-  int csubi = *csub.getIm();
-
-  cout << csubr << "\t" << csubi << endl;
+  cout << csub << endl;
   // up to this point.. complex subtraction appears to work.
 
   // now need to test Quaternion +, -, and *
-
+/*
   Quaternion quaa = qua + qua2;
   Quaternion quas = qua - qua2;
-  /// works up to this point
-  
+ */ /// works up to this point
+  /*
   Quaternion quam = qua * qua2;
 
   Complex *cm1, *cm2;
@@ -89,11 +61,11 @@ int main() {
   am1i = am1.getIm();
   am2r = am2.getRe();
   am2i = am2.getIm();
-  // quaternion mult compiles.. but now need to look at entries to see if correct 
+ */ // quaternion mult compiles.. but now need to look at entries to see if correct 
 
-  cout << *am1r << "\t" << *am1i << endl
+ /* cout << *am1r << "\t" << *am1i << endl
     << *am2r << "\t" << *am2i << endl;
-  /*
+ */ /*
   Octonion oct(quat1, quat2);
 */
   return 0;
